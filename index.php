3923261fef4a2d4511684dafa1526de80d5cf9ec
Anubhab Sen <anubhabsen@gmail.com>
2016-12-18 10:50:24 +0530

    Function placed in different folder

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-function get_curl($url)
-{
-	$curl_handle=curl_init();
-	curl_setopt($curl_handle, CURLOPT_URL, $url);
-	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 10);
-	$result= curl_exec($curl_handle);
-	return $result;
-}
 
+include_once 'func/function.inc.php';
 
 $res = get_curl("https://www.goodreads.com/quotes_of_the_day");
 
