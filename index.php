9d7f0a6e43b2053b23c00ca9f7e529fe44284c48
Anubhab Sen <anubhabsen@gmail.com>
2016-12-18 11:08:39 +0530

    Adds basic HTML

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<?php
-
-include_once 'func/function.inc.php';
-
-$res = get_curl("https://www.goodreads.com/quotes_of_the_day");
-
-$start = strpos($res, '<div class=\'quote\'>');
-$end = strpos($res,'<div class="quoteFooter">', $start);
-$length = $end-$start;
-
-$res = substr($res, $start, $length);
-$res=str_replace("href=", "",$res);
-
-echo ($res);
-?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Quote of the day</title>
+  <meta name="Anubhab Sen">
+</head>
+<body>
+	<h1>Quote of the day</h1>
+	<h3>Brought to you by GoodReads</h3>
+	<br>
+	<?php
+	include_once 'func/function.inc.php';
+	$res = get_curl("https://www.goodreads.com/quotes_of_the_day");
+	$start = strpos($res, '<div class=\'quote\'>');
+	$end = strpos($res,'<div class="quoteFooter">', $start);
+	$length = $end-$start;
+	$res = substr($res, $start, $length);
+	$res=str_replace("href=", "",$res);
+	echo ($res);
+	?>
+</body>
+</html>
