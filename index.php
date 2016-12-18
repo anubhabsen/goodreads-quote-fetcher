82f9716d0d65e42d1399b0fc54267c0725f6da9f
Anubhab Sen <anubhabsen@gmail.com>
2016-12-18 23:47:35 +0530

    Adds context for the quote, goodreads logo and some styling

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	include_once'func/function.inc.php';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,18 +10,33 @@
   <meta name="Anubhab Sen">
 </head>
 <body>
-	<h1>Quote of the day</h1>
-	<h3>Brought to you by GoodReads</h3>
+	<div style="text-align: center;">
+		<img src="images/goodreads.jpg">
+		<h1>Quote of the day</h1>
+	</div>
 	<br>
-	<?php
-	include_once 'func/function.inc.php';
-	$res = get_curl("https://www.goodreads.com/quotes_of_the_day");
-	$start = strpos($res, '<div class=\'quote\'>');
-	$end = strpos($res,'<div class="quoteFooter">', $start);
-	$length = $end-$start;
-	$res = substr($res, $start, $length);
-	$res=str_replace("href=", "",$res);
-	echo ($res);
-	?>
+	<div style="text-align: center;">
+		<?php
+		$res = get_curl("https://www.goodreads.com/quotes_of_the_day");
+		$start = strpos($res, '<div class=\'quote\'>');
+		$end = strpos($res,'<div class="quoteFooter">', $start);
+		$length = $end-$start;
+		$res = substr($res, $start, $length);
+		$res=str_replace("href=", "",$res);
+		echo ($res);
+		?>
+	</div>
+	<br><br><br>
+	<div style="text-align: center;">
+		<?php
+		$res = get_curl("https://www.goodreads.com/quotes_of_the_day");
+		$start = strpos($res, 'About this quote');
+		$end = strpos($res,'<div class="bigGreyBoxBottom">', $start);
+		$length = $end-$start;
+		$res = substr($res, $start, $length);
+		$res=str_replace("href=", "",$res);
+		echo ($res);
+		?>
+	</div>
 </body>
 </html>
